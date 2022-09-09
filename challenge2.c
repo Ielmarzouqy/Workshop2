44b9854b1e0aacfc5cfa9c512fcae16d2f08c7fc
Ielmarzouqy <elmarzouqyimane@gmail.com>
2022-09-09 10:33:24 +0100

    challenge 2

---
+++ challenge2.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+
+//une voyelle ou non 
+//switch case
+char lettre;
+
+printf(" entrer une lettr: ");
+scanf("%c", &lettre);
+switch (lettre)
+{
+case  'a'/* constant-expression */:
+    /* code */
+    {printf(" la lettre est une voyelle %c", lettre);}
+    break;
+case  'o':
+    {printf(" la lettre est une voyelle %c", lettre);}
+
+    break;
+
+    case  'e':
+    {printf(" la lettre est une voyelle %c", lettre);}
+
+    break;
+
+    case  'i':
+    {printf(" la lettre est une voyelle %c", lettre);}
+
+    break;
+    case  'u':
+    {printf(" la lettre est une voyelle %c", lettre);}
+
+    break;
+    default:
+    printf(" la lettre est une consonnes %c", lettre);
+}
+
+    return 0;
+}
