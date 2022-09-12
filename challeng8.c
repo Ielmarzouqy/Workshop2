bd5fd1d9276bef9009627f57f6591feb3a513da5
Ielmarzouqy <elmarzouqyimane@gmail.com>
2022-09-12 11:51:33 +0100

    challenge 8 changer la valeur de a avec la valeur de b en utilisant les pointers

---
+++ challeng8.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+
+int main(){
+// int a, b;
+// int* ptr;
+// printf("donner la valeur de a :");
+// scanf("%d", &a);
+
+// printf(" donner la valeur de b :");
+// scanf("%d", &b);
+// *ptr = a ;
+// a=b;
+// b= *ptr;
+// printf("la valeur de a est %d", a);
+// printf(" et la valeur de b est %d", *ptr);
+int a;
+int b;
+int nombres[2];
+
+printf("donner le nombre 1 :");
+scanf("%d", &a);
+printf("donner le nombre 2 :");
+scanf("%d", &b);
+
+*nombres = b;
+*(nombres +1) = a;
+printf("premier nombre  : %d\n", *nombres);
+printf("premier nombre  : %d\n", *(nombres +1));
+
+    return 0;
+}3
